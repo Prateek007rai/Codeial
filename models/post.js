a3515b9c67d5983fdf8154ec3d726d71c1af0e9c
unknown <prateekrai341036@gmail.com>
2022-08-31 12:33:56 +0530

    post created and displyaed well

--- models/post.js
+++ models/post.js
@@ -1,4 +1,4 @@
-const mongoose = require('mngoose');
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
     content: {
@@ -13,6 +13,6 @@ const postSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const Post = mongoose.model('User' , postSchema);
+const Post = mongoose.model('Post' , postSchema);
 
 module.exports = Post;
